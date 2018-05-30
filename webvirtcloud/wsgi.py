03ed5aea455ea36c755c0c69a98cb537645e1465
HongWeipeng <hongweichen8888@sina.com>
2018-05-30 18:19:00 +0800

    多进程方式运行vnc后台服务

--- webvirtcloud/wsgi.py
+++ webvirtcloud/wsgi.py
@@ -12,15 +12,21 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webvirtcloud.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
+from vrtManager import websockify
 
 
 # noVNC server
+from webvirtcloud.settings import WS_PORT
+from webvirtcloud.settings import WS_PUBLIC_HOST
+from webvirtcloud.settings import VNC_TOKENS_FILE
 import multiprocessing
 
 def worker():
-    cmd = 'python noVNC/utils/websockify --target-config=vnc_tokens 6080 >/dev/null 2>&1'
-    os.system(cmd)
+    server = websockify.WebSocketProxy(listen_port=WS_PORT, target_cfg=VNC_TOKENS_FILE,)
+    server.start_server()
+
+    #cmd = 'python noVNC/utils/websockify --target-config=vnc_tokens 6080 >/dev/null 2>&1'
+    #os.system(cmd)
 
 def start_websockify():
     t = multiprocessing.Process(target=worker)
